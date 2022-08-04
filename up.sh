ce63e19b440dc2a700a720506d0531802b41cded
M. Gschwandtner <84477901+OnlyPain-ctrl@users.noreply.github.com>
2022-08-04 20:32:11 +0200

    added deploy check depending on git changes

--- up.sh
+++ up.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/bash
 
+check=$(git pull | grep -c "Already up to date.")
+
+if [ "$check" = 1 ];
+then
+    echo "nothing to update" && exit
+fi
+
 cd ./dRun &&
     ./kuma.sh &&
     ./portainer.sh &&
