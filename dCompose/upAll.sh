46e194b9cfec05d10fc64e279dabe58f00d5ac09
M. Gschwandtner <84477901+OnlyPain-ctrl@users.noreply.github.com>
2022-08-15 22:07:32 +0200

    added options to up script

--- dCompose/upAll.sh
+++ dCompose/upAll.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/bash
 
 cd ./automation &&
-    docker-compose pull &&
+    [ "$1" != "-nopull" ] && docker-compose pull
     docker-compose up -d
 
 cd ../static &&
     mkdir -p public &&
-    docker-compose pull &&
+    [ "$1" != "-nopull" ] && docker-compose pull
     docker-compose up -d &&
     cd ./public &&
     ls | xargs -I{} git -C {} pull
--- up.sh
+++ up.sh
@@ -8,24 +8,43 @@ mkdir -p ./dCompose/automation/static/public &&
     cd ../../../../
 
 check=$(git status | grep -c "Your branch is up to date with")
+dpull=0
+git=0
 
-if [ "$1" = "-f" ]; then
-    echo "force update" && check=999
-fi
+for arg in "$@"; do
+    if [ "$arg" = "-f" ]; then
+        echo "force update" && check=999
+    fi
+    if [ "$arg" = "-dpull" ]; then
+        echo "pull docker images" && dpull=1
+    fi
+    if [ "$arg" = "-git" ]; then
+        echo "pull docker images" && git=1
+    fi
+done
 
 if [ "$check" = 1 ]; then
     echo "nothing to update" && exit
 fi
 
-git stash &&
-    git pull
+if [ "$git" = 1 ]; then
+    git stash &&
+        git pull
+fi
 
-cd ./dRun &&
-    ./kuma.sh &&
-    ./portainer.sh &&
-    echo "(!!) dRun updated"
+if [ "$dpull" = 1 ]; then
+    cd ./dRun &&
+        ./kuma.sh &&
+        ./portainer.sh &&
+        echo "(!!) dRun updated"
 
-cd ../dCompose &&
-    ./upAll.sh &&
-    [[ -f ./postUp.sh ]] && ./postUp.sh &&
-    echo "(!!) dCompose updated"
+    cd ../dCompose &&
+        ./upAll.sh &&
+        [[ -f ./postUp.sh ]] && ./postUp.sh &&
+        echo "(!!) dCompose updated"
+else
+    cd ./dCompose &&
+        ./upAll.sh -nopull &&
+        [[ -f ./postUp.sh ]] && ./postUp.sh &&
+        echo "(!!) dCompose updated"
+fi
